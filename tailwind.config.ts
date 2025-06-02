518ed6df1af4bc81a856b7d6e505ab44e3a5923d
alexserban97 <84380047+alexserban97@users.noreply.github.com>
2025-06-02 16:27:51 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,7 +70,7 @@ export default {
 				wedding: {
 					rose: '#f58a77',
 					'rose-dark': '#e56b61',
-					gold: '#a9186e',
+					gold: '#b38600',
 					cream: '#faf7f0',
 					sage: '#b5c99a',
 				}
